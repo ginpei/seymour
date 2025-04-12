11f3b9df8f4352fefaef09a402acdfdc72422353
Ginpei <ginpei@ginpei.dev>
2025-04-11 23:51:39 -0700

    accept dir path

--- src/subCommands/generate.ts
+++ src/subCommands/generate.ts
@@ -1,13 +1,16 @@
-import { generateDocumentChunks } from "../lib/generator";
+import { existsSync, statSync } from "node:fs";
 import { writeChunks } from "../lib/files";
+import { generateDocumentChunks } from "../lib/generator";
 
-export async function generate(pattern: string) {
+export async function generate(pathOrPattern: string) {
   process.stdout.write(`Embedding...\r`);
+
+  const pattern = pathToPattern(pathOrPattern);
 
   const chunks = await generateDocumentChunks({
     cacheDir: './cache',
     OPENAI_API_KEY: process.env.OPENAI_API_KEY || '',
-    pattern: pattern,
+    pattern,
     onEmbedProgress: (index, length) => {
       process.stdout.write(`Embedding ${index} / ${length} (${((index / length) * 100).toFixed(2)}%)\r`);
     },
@@ -17,4 +20,15 @@ export async function generate(pattern: string) {
 
   writeChunks(chunks);
   console.log(`Generated ${chunks.length} chunks`);
+}
+
+function pathToPattern(input: string) {
+  if (existsSync(input)) {
+    const status = statSync(input);
+    if (status.isDirectory()) {
+      return `${input}${input.endsWith("/") ? "" : "/"}**/*.md`;
+    }
+  }
+
+  return input;
 }
